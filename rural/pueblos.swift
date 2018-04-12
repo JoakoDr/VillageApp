ffe82e2ef67284cd478366c62f0582e6cbb549e8
JoaquinDiaz <joaco_diaz_ramirez@hotmail.com>
2018-04-12 01:29:28 -0700

    Funcional, se muestran los pueblos

---
+++ rural/pueblos.swift
@@ -0,0 +1,13 @@
+//
+//  pueblos.swift
+//  rural
+//
+//  Created by JOAQUIN DIAZ RAMIREZ on 12/4/18.
+//  Copyright © 2018 DAVID ANGULO CORCUERA. All rights reserved.
+//
+
+import UIKit
+
+class pueblos: NSObject {
+
+}
